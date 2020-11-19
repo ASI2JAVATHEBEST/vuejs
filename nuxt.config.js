f7dc245ba2e92da96c204b4f38b03b8e433dcdd0
Gautier <gautier.deleglise@ediservices.com>
2020-11-19 14:58:37 +0100

    e

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,7 +43,7 @@ export default {
   proxy: {
     '/socket.io': 'http://localhost:3001',
     '/user': 'http://localhost:8084',
-    '/cards': 'http://localhost:8082'
+    '/card': 'http://localhost:8082'
   },
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
